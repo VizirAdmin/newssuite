b73aaf1b21f0a7ad050dfc3c5d50a8470182eebc
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2011-03-18 13:25:14 -0300

    adicionado analytics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def google_analytics
+    render :partial => 'shared/analytics' if Rails.env == 'production'
+  end
 end
+
